6939cfaae6fdedbb6d6efec6ac9252ba66573a32
Eric Lee <ranye0719@gmail.com>
2019-03-01 17:44:32 +0800

    weather has been added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const request = require('request');
 const yargs = require('yargs');
 
 const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 const argv = yargs.options({
   a: {
@@ -13,15 +14,33 @@ const argv = yargs.options({
   k: {
     demand: true,
     alias: 'apiKey',
-    describe: 'apiKey for fetching data',
+    describe: 'apiKey for fetching geolocation.',
+    string: true
+  },
+  l: {
+    demand: true,
+    alias: 'apiKey2',
+    describe: 'apiKey for fetching weather.',
     string: true
   }
 }).help().alias('help', 'h').argv;
 
 geocode.geocodeAddress(argv, (errMsgs, results) => {
-  if(errMsgs) {
+  if (errMsgs) {
     console.log(errMsgs);
   } else {
-    console.log(results);
+    const weatherReq = {
+      apiKey2: argv.apiKey2,
+      lat: results.lat,
+      lng: results.lng
+    };
+    weather.getWeather(weatherReq, (errMsgs, results) => {
+      if (errMsgs) {
+        console.log(errMsgs);
+      } else {
+        console.log(argv.address);
+        console.log(`It's currently ${results.temperature}. It feels like ${results.apparentTemperature}.`)
+      }
+    })
   }
 });
