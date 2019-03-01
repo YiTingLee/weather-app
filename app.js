44f22b9ebfbc0a8ac0ec0deb337d88f7b5497b7c
Eric Lee <ranye0719@gmail.com>
2019-03-01 16:14:50 +0800

    yargs has been added

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const request = require('request');
-const apiKey = process.argv[2];
+const yargs = require('yargs');
+
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  },
+  k: {
+    demand: true,
+    alias: 'apiKey',
+    describe: 'apiKey for fetching data',
+    string: true
+  }
+}).help().alias('help', 'h').argv;
+
+console.log('argv:' + JSON.stringify(argv));
+const encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${apiKey}&location=1301%20lombard%20street%20philadelphi`,
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=${argv.apiKey}&location=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log('result:' + JSON.stringify(body.results));
